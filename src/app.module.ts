0e6098096a0b06b6cb047604ff6e7dae76be8aa0
ZEHNSH28 <jenith@crowdwisdom360.com>
2023-05-25 10:26:38 +0530

    commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ dotenv.config();
 
 @Module({
   imports: [
-   
+    MongooseModule.forRoot(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }),
     CryptocurrencyRatingModule,
   ],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { getConnectionToken } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
 
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
+
+  // Listen for MongoDB connection errors
+  const mongoConnection: Connection = app.get(getConnectionToken());
+  mongoConnection.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
   await app.listen(port);
   console.log(`Application is running on: http://localhost:${port}`);
 }
